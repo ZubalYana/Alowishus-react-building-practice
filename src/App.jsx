e8752afea1d3bd054397bbb341987facdd2381e3
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-07 06:44:04 +0300

    create the main page content left container

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,16 @@ function App() {
     <div className="wrap">
       <div className="mainPage">
         <Header />
+        <div className="mainPage_content">
+          <div className="main_leftPart">
+            <h1 className='main_title'>Alowishus <br /> Delicious <br />Coffee </h1>
+            <h4 className='main_subtitle'>A drink from the ‘My Alowishus’ bottled brews <br /> range OR grab one of our delicious coffee’s.</h4>
+            <div className="main_buttons">
+              <div className="downloadBtn">Download App</div>
+              <div className="shopCoffeeBtn">Shop Coffee <div className="shopCoffeeBtnHr"></div></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     </>
