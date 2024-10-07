e5e44d4961ea10e362f978fe0e149dc88b75b001
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-07 06:27:15 +0300

    fix mistakes and display the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-
+import Header from './Components/Header/Header';
 function App() {
 
   return (
     <>
+    <div className="wrap">
+      <div className="mainPage">
+        <Header />
+      </div>
+    </div>
     </>
   )
 }
