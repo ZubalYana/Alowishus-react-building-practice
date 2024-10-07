ea27c04a875d285370f98c2bf9866347dbcd6434
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-07 12:00:51 +0300

    set the basic explore cards styles

--- src/Components/Explore_card/Explore_card.jsx
+++ src/Components/Explore_card/Explore_card.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './Explore_card.css'
 
-export default function Explore_card() {
+export default function Explore_card(title, subtitle, img, btnText) {
     return (
         <div className="Explore_card">
             <div className="Explore_card_text">
-                <h3 className="Explore_card_title"></h3>
-                <p className="Explore_card_subtitle"></p>   
+                <h3 className="Explore_card_title">{title}</h3>
+                <p className="Explore_card_subtitle">{subtitle}</p>   
             </div>
-            <img className="Explore_card_img" src="" alt="" />
-            <div className="Explore_cardBtn">Order cathering</div>
+            <img className="Explore_card_img" src={img} alt="" />
+            <div className="Explore_cardBtn">{btnText}</div>
+
         </div>
     )
 }
