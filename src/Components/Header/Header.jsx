8c594dec2bbfb76e08ff862b4dbed12dc2bd822e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-07 06:18:49 +0300

    fill header content

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css'
+import logo from '../../assets/logo.png'
+import cartSvg from '../../assets/cart.svg'
+export default function Header() {
+    return (
+        <div className="Header">
+            <img src={logo} alt="" />
+            <ul className="nav">
+                <li>Cafe Munu</li>
+                <li>About Us</li>
+                <li>Find Us</li>
+                <li>Alowishus Catering</li>
+            </ul>
+            <div className="cartBtn">
+                <img src={cartSvg} alt="" />
+            </div>
+            <div className="buyBtn">Buy gift vouchers</div>
+        </div>
+    );
+}
