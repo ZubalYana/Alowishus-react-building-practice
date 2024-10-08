45a29de77f37e6e9db9cfa3bc0db9e1b28cc93e1
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-08 14:13:17 +0300

    set the cards general apperience and styles

--- src/Components/BestCoffee_card/BestCoffee_card.jsx
+++ src/Components/BestCoffee_card/BestCoffee_card.jsx
@@ -4,17 +4,19 @@ export default function BestCoffee_card({img, numberSelling, coffeeName}) {
     return(
         <div className="BestCoffeeCard">
             <div className="card_topPart">
+                <div className="imgCon">
                 <img className="card_img" src={img} alt="" />
+                </div>
                 <div className="card_topText">
                     <div className="card_numberSelling">{numberSelling}</div>
                     <div className="card_coffeeName">{coffeeName}</div>
                 </div>
-                <div className="card_text">Amet minim mollit non deserunt dolor ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</div>
+            </div>
+            <div className="card_text">Amet minim mollit non deserunt dolor ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</div>
                 <div className="card_priceBtnCon">
                     <div className="card_coffeePrice">$59.99</div>
                     <div className="card_orderBtn">Order now</div>
                 </div>
-            </div>
         </div>
     )
 }
