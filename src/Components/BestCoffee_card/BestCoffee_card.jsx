d408bb777e9322b8b9fec154dbdfd29d84c0585f
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-10-08 13:57:11 +0300

    fill all the card component content

---
+++ src/Components/BestCoffee_card/BestCoffee_card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function BestCoffee_card(){
+    return(
+        <div className="BestCoffeeCard">
+            <div className="card_topPart">
+                <img className="card_img" src="" alt="" />
+                <div className="card_topText">
+                    <div className="card_numberSelling"></div>
+                    <div className="card_coffeeName"></div>
+                </div>
+                <div className="card_text">Amet minim mollit non deserunt dolor ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</div>
+                <div className="card_priceBtnCon">
+                    <div className="card_coffeePrice">$59.99</div>
+                    <div className="card_orderBtn">Order now</div>
+                </div>
+            </div>
+        </div>
+    )
+}
